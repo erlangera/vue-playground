46f00a78df1c5a5f42b5b9287549834f722e7c6c
erlangera <1638442068@qq.com>
2022-06-07 01:08:48 +0800

    修改目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,17 @@
 module.exports = {
-  title: "Vue 源码",
-  description: "Vue源码实验例子",
+  title: 'Vue 源码',
+  description: 'Vue源码实验例子',
+  base: '/vue-playground',
   themeConfig: {
-    nav: [
-      { text: "Vue 2.x", link: "/v2/" },
-    ],
+    nav: [{ text: 'Vue 2.x', link: '/v2/' }],
     sidebar: {
-      "/v2/": [
+      '/v2/': [
+        '',
         {
-          title: "数据驱动",
+          title: '数据驱动',
           collapsable: false,
-          children: [
-            ['reactive/', 'Introduction']
-          ],
-        }
+          children: [['reactive/', 'Introduction']],
+        },
       ],
     },
   },
