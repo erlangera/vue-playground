58fa35b2adeb9cca0573d225a002080eb9451e0e
erlangera <1638442068@qq.com>
2022-06-06 20:04:51 +0800

    vuepress框架搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "Vue 源码",
+  description: "Vue源码实验例子",
+  themeConfig: {
+    nav: [
+      { text: "Vue 2.x", link: "/v2/" },
+    ],
+    sidebar: {
+      "/v2/": [
+        {
+          title: "数据驱动",
+          collapsable: false,
+          children: [
+            ['reactive/', 'Introduction']
+          ],
+        }
+      ],
+    },
+  },
+};
---
+++ docs/v2/index.md
@@ -0,0 +1,4 @@
+# 前言
+本文主要为学习[《深入浅出Vue.js》](https://www.ituring.com.cn/book/2675)、[Vue.js 技术揭秘 V2.x版本](https://ustbhuangyi.github.io/vue-analysis/)过程中一些验证实验例子。
+
+使用的Vue源码版本为的fork自源码的[2.6.14](https://github.com/erlangera/vue/tree/study)，该版本已使用Typescript替代了flow，为了方便演示例子自定义的编译版本。
